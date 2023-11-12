0521f531253038ffa43afeb80ae9e20c2a2a12cd
Gyumin Hwang <gugu26@naver.com>
2023-11-12 20:44:49 +0900

    third commit

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,7 +4,6 @@ from pybo.views.main_views import bp as main_bp
 def create_app():
     app = Flask(__name__)
 
-    from .views import main_views
-    app.register_blueprint(main_views.bp)
+    app.register_blueprint(main_bp)
 
     return app
